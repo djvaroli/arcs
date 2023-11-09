14d74406329552bded4a2a9f32ccd0c1584d27a5
Daniel Varoli <daniel.varoli@gmail.com>
2023-11-09 00:18:28 -0800

    Changed name

--- landing.py
+++ landing.py
@@ -2,8 +2,8 @@ import io
 
 import streamlit as st
 
-title = "GPT4-Live"
+title = "Arcs - a collection of interactive GPT demos!"
 st.set_page_config(page_title=title)
 
 st.title(title)
-st.write("Examples of GPT4-Live in action.")
+st.write("Click on a page on the left to get started!")
